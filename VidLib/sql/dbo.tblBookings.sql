611cc374c53c94e06f4a7ab5aeed847df46b7b1c
MeObviously <52016578+MeObviously@users.noreply.github.com>
2021-09-08 18:45:48 +1200

    Add project files.

---
+++ VidLib/sql/dbo.tblBookings.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[tblBookings] (
+    [Id] INT IDENTITY(0,1) NOT NULL,
+	[Name] VARCHAR(50) NOT NULL,
+	[Time] TIME NOT NULL,
+	[Date] DATE NOT NULL,
+	[Seats] INTEGER NOT NULL,
+	[Phone] VARCHAR(12) NOT NULL,
+	[Email] VARCHAR(150) NOT NULL,
+	[Category] VARCHAR(14) NOT NULL,
+
+
+
+
+
+
+
+
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ VidLib/sql/dbo.tblBookings_insert.sql
@@ -0,0 +1,13 @@
+﻿		INSERT INTO [dbo].[tblBookings] ([Name], [Time], [Date], [Seats], [Phone], [Email], [Category]) VALUES ( 'Maron',  '18:00:00',  '2018-10-07', 2,  '0277971641',  'maron@fakemail.com',  'VIP')
+		INSERT INTO [dbo].[tblBookings] ([Name], [Time], [Date], [Seats], [Phone], [Email], [Category]) VALUES ( 'Fey',  '19:00:00',  '2018-10-07', 3,  '0277971642',  'fey@notreal.co.nz',  'Regular')
+		INSERT INTO [dbo].[tblBookings] ([Name], [Time], [Date], [Seats], [Phone], [Email], [Category]) VALUES ( 'Gai',  '19:15:00',  '2018-10-07', 4,  '022222222',  'sumgai@gmail.ca',  'VIP')
+		INSERT INTO [dbo].[tblBookings] ([Name], [Time], [Date], [Seats], [Phone], [Email], [Category]) VALUES ( 'Connolly',  '19:00:00',  '2018-10-07', 2,  '0277971643',  'Connolly@pretendmail.co.nz',  'Casual')
+		INSERT INTO [dbo].[tblBookings] ([Name], [Time], [Date], [Seats], [Phone], [Email], [Category]) VALUES (  'Rock',  '20:00:00',  '2018-10-07', 12,  '0277971644',  'hard@pretendmail.co.nz',  'Casual')
+		INSERT INTO [dbo].[tblBookings] ([Name], [Time], [Date], [Seats], [Phone], [Email], [Category]) VALUES ( 'Goldberg',  '18:00:00',  '2018-07-11', 4,  '0277971645',  'golden@fakemail.com',  'Regular')
+		INSERT INTO [dbo].[tblBookings] ([Name], [Time], [Date], [Seats], [Phone], [Email], [Category]) VALUES (  'Seinfeld',  '19:00:00',  '2018-07-11', 4,  '0277971646',  'seinfeild@notreal.co.nz',  'VIP')
+		INSERT INTO [dbo].[tblBookings] ([Name], [Time], [Date], [Seats], [Phone], [Email], [Category]) VALUES ( 'Idle',  '19:00:00',  '2018-07-11', 2,  '0277971647',  'Helen@fakemail.com',  'Regular')
+		INSERT INTO [dbo].[tblBookings] ([Name], [Time], [Date], [Seats], [Phone], [Email], [Category]) VALUES ( 'Szekely',  '20:00:00',  '2018-07-11', 6,  '0277971648',  'james@pretendmail.co.nz',  'Casual')
+		INSERT INTO [dbo].[tblBookings] ([Name], [Time], [Date], [Seats], [Phone], [Email], [Category]) VALUES ( 'Kramer',  '20:00:00',  '2018-07-11', 4,  '0277971646',  'kramer@notreal.co.nz',  'Regular')
+	
+	
+	
