e2e13cac373f9268b41925e0012dd5e5fa48decb
MeObviously <52016578+MeObviously@users.noreply.github.com>
2021-09-19 16:45:40 +1200

    S2_Add_tblRentals

---
+++ VidLib/sql/dbo.tblRentals_create.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblRentals]
+(
+	[Rental_Id] INT IDENTITY(0,1) NOT NULL,
+	[Name] VARCHAR(50) NOT NULL,
+	[Phone] VARCHAR(12) NOT NULL,
+	[Email] VARCHAR(50) NOT NULL,
+	[Rental_Date] DATE NOT NULL,
+	[Rental_Days] INTEGER NOT NULL,
+	[Return_Date] DATE NOT NULL,
+	[Title] VARCHAR(50) NOT NULL,
+	[Genre] VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY CLUSTERED ([Rental_Id])
+)
