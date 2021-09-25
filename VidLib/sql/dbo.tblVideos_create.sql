4ade453cb8f0d9598f2492c56522c3eac7e2b369
MeObviously <52016578+MeObviously@users.noreply.github.com>
2021-09-26 09:45:10 +1300

    videos page execute query working

---
+++ VidLib/sql/dbo.tblVideos_create.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblVideos] (
+    [Video_Id]    INT IDENTITY (0, 1) NOT NULL,
+    [Video_Title] VARCHAR (50) NOT NULL,
+    [Video_Genre] VARCHAR (20) NOT NULL,
+    [Year]        INT          NOT NULL,
+    [Director]    VARCHAR (20) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Video_Id] ASC)
+);
